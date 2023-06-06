d38999663b4e22bf2cde7240f4ca5447909fd659
joelcoronah <joelcoronah@gmail.com>
2023-06-06 17:43:11 -0400

    :sparkless: feat: add firebase login template

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,52 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import * as admin from 'firebase-admin';
+import { ServiceAccount } from 'firebase-admin';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+
+  /**
+   *  Cors config
+   */
+  const cors = {
+    origin: [
+      'http://localhost:3000',
+      'https://serene-kepler-792adb.netlify.app',
+    ],
+    methods: 'GET, HEAD, PUT, PATCH, POST, DELETE, OPTIONS',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true,
+    allowedHeaders: [
+      'Accept',
+      'Content-Type',
+      'Authorization',
+      'Cookie',
+      'language',
+      'token',
+      'refreshToken',
+    ],
+  };
+
+  const adminConfig: ServiceAccount = {
+    projectId: configService.get<string>('FIREBASE_PROJECT_ID'),
+    privateKey: configService
+      .get<string>('FIREBASE_PRIVATE_KEY')
+      .replace(/\\n/g, '\n'),
+    clientEmail: configService.get<string>('FIREBASE_CLIENT_EMAIL'),
+  };
+
+  // Initialize the firebase admin app
+  admin.initializeApp({
+    credential: admin.credential.cert(adminConfig),
+  });
+
+  app.enableCors(cors);
+
+  await app.listen(configService.get('PORT'));
 }
+
 bootstrap();
